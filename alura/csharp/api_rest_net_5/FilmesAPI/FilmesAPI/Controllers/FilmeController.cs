7f5f457b5936edae43252fc6f3b666e470760ecf
mthmf <maathmf@gmail.com>
2021-09-16 14:20:36 -0300

    Inicio curso Api net5

---
+++ alura/csharp/api_rest_net_5/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> Filmes = new List<Filme>();
+        private static int Id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = Id++;
+            Filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return Filmes;
+        }
+
+        [HttpGet]
+        [Route("/{id}")]
+        public Filme RecuperarFilmePorId([FromQuery] int id)
+        {
+            return Filmes.FirstOrDefault(f => f.Id == id);
+        }
+
+
+    }
+}
