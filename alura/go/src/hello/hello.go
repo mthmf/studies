040b98efdfb052176c72e4bf5d9ea94e2f305fb8
mthmf <maathmf@gmail.com>
2023-05-16 07:24:45 -0300

    controle de fluxo, if/switch

--- alura/go/src/hello/hello.go
+++ alura/go/src/hello/hello.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	//fmt.Println("Hello World")
 	var nome string = "Yang"
-	var idade int = 32
 	var versao float32 = 1.1
-	var ves = 2.1 // float64 = pega sempre o maior.
-	fmt.Println("Hello. Mrs.", nome, "sua idedade é ", idade)
+	//var ves = 2.1 // float64 = pega sempre o maior.
+	//fmt.Println("Tipo da variavel:", reflect.TypeOf(ves))
+
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Versão do sistema: ", versao)
 
-	fmt.Println("Tipo da variavel:", reflect.TypeOf(ves))
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair")
+	var comando int
+	fmt.Scan(&comando)
+
+	//if comando == 1 {
+	//	fmt.Println("Monitorando...")
+	//}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando")
+	case 2:
+		fmt.Println("Exibindo Logs")
+	default:
+		fmt.Println("Comnando não encontrado.")
+	}
 }
