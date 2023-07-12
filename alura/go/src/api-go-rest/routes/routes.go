63174bfeefd94ea74ce56a1df632243efc4eca91
mthmf <maathmf@gmail.com>
2023-07-12 18:41:44 -0300

    go api rest aula 5

--- alura/go/src/api-go-rest/routes/routes.go
+++ alura/go/src/api-go-rest/routes/routes.go
@@ -2,14 +2,17 @@ package routes
 
 import (
 	"go-rest-api/controllers"
+	"go-rest-api/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	var r = mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("GET")
@@ -17,5 +20,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("DELETE")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("PUT")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
